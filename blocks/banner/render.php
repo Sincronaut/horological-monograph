1ade66bd631ce9f6734d2a3419eb2b8c736e5e1f
Sincronaut <jmabcdefg1004@gmail.com>
2026-03-27 13:34:44 +0800

    Created banner block for hero sections

---
+++ blocks/banner/render.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * Banner Block Render Template
+ * 
+ * @var array    $attributes Block attributes.
+ * @var string   $content    Block inner content.
+ * @var WP_Block $block      Block instance.
+ */
+
+$subtitle = $attributes['subtitle'] ?? '';
+$title = $attributes['title'] ?? '';
+$buttonText = $attributes['buttonText'] ?? '';
+$buttonUrl = $attributes['buttonUrl'] ?? '';
+$background_image = $attributes['backgroundImage'] ?? '';
+
+// Handle theme-relative paths (e.g., /assets/images/...)
+if ( ! empty( $background_image ) && ! str_starts_with( $background_image, 'http' ) ) {
+    // If it starts with /assets but should be in the theme folder
+    if ( str_starts_with( $background_image, '/' ) ) {
+        // Strip leading slash to make it relative to theme directory
+        $background_image = ltrim( $background_image, '/' );
+    }
+    
+    // Construct dynamic theme URL
+    $background_image = get_stylesheet_directory_uri() . '/' . $background_image;
+}
+
+$style = '';
+if ($background_image) {
+    $style = ' style="background-image: url(' . esc_url($background_image) . ');"';
+}
+?>
+
+<div class="hm-banner">
+    <div class="hm-banner__background" <?php echo $style; ?>></div>
+    <div class="hm-banner__overlay"></div>
+    <div class="hm-banner__content">
+        <?php if ($subtitle) : ?>
+            <p class="hm-banner__subtitle"><?php echo esc_html($subtitle); ?></p>
+        <?php endif; ?>
+
+        <?php if ($title) : ?>
+            <h1 class="hm-banner__title"><?php echo wp_kses_post(nl2br($title)); ?></h1>
+        <?php endif; ?>
+
+        <?php if ($buttonText) : ?>
+            <div class="hm-banner__actions">
+                <a href="<?php echo esc_url($buttonUrl); ?>" class="hm-banner__button">
+                    <?php echo esc_html($buttonText); ?>
+                </a>
+            </div>
+        <?php endif; ?>
+    </div>
+</div>
---
+++ functions.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * Child Theme Functions
+ *
+ * @package child-horological-monograph
+ */
+
+/**
+ * Register custom blocks from the /blocks directory.
+ */
+function chm_register_custom_blocks() {
+    $blocks_path = get_stylesheet_directory() . '/blocks/';
+    
+    if ( ! is_dir( $blocks_path ) ) {
+        return;
+    }
+
+    $blocks = scandir( $blocks_path );
+
+    foreach ( $blocks as $block_name ) {
+        if ( $block_name === '.' || $block_name === '..' ) {
+            continue;
+        }
+
+        $block_dir = $blocks_path . $block_name;
+
+        if ( is_dir( $block_dir ) && file_exists( $block_dir . '/block.json' ) ) {
+            register_block_type_from_metadata( $block_dir );
+        }
+    }
+}
+add_action( 'init', 'chm_register_custom_blocks' );
+
+/**
+ * Enqueue theme styles.
+ */
+function chm_enqueue_styles() {
+    // Parent theme styles.
+    wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+    
+    // Child theme styles.
+    wp_enqueue_style( 'child-style', get_stylesheet_uri(), array( 'parent-style' ), wp_get_theme()->get( 'Version' ) );
+}
+add_action( 'wp_enqueue_scripts', 'chm_enqueue_styles', 20 );
+
+/**
+ * Enqueue editor styles for Google Fonts.
+ */
+function chm_editor_styles() {
+    add_editor_style( 'style.css' );
+}
+add_action( 'after_setup_theme', 'chm_editor_styles' );
